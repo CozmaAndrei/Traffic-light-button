9168ade069ff7867cde3954726eab94c557548d4
CozmaAndrei <133941110+CozmaAndrei@users.noreply.github.com>
2023-11-08 16:15:23 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const btn = document.getElementById('button');
+let index = 0;
+const colors = ['yellow', 'green', 'red'];
+
+function changeButtonColor() {
+    btn.style.backgroundColor = colors[index];
+    ++index;
+    if (index == colors.length) {
+        index = 0;
+    }
+};
+setInterval(changeButtonColor,10000);
